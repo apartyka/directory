a12cf66e5b943d82445998e0c05ddbdca75de9c5
Adam Partyka <adampartyka@gmail.com>
2016-09-07 11:27:25 -0400

    stub out main app component that will hold AddProvider and ProviderList

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,25 @@
-// ...coming soon
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-console.debug('Hello World!');
+class App extends React.Component {
+  render() {
+    return (
+    	<div className="col-sm-12">
+        Hello {this.props.name}
+        <div className="addProvider">
+          Add Provider form component...
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            Provider list component here...
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App name="Adam" />,
+  document.getElementById('app')
+);
