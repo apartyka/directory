60898a79f182c1eb9ee0a5e2c02c449cdd018c89
Adam Partyka <adampartyka@gmail.com>
2016-09-08 16:36:42 -0400

    ugh...safari is stupid, had to change all const declarations to var

--- src/js/AddProvider.js
+++ src/js/AddProvider.js
@@ -7,11 +7,11 @@ class AddProvider extends React.Component {
   }
 
   clearForm() {
-    const fields = ['fName', 'lName', 'email', 'specialty', 'practiceName'];
+    var fields = ['fName', 'lName', 'email', 'specialty', 'practiceName'];
 
-    fields.map(field => {
+    fields.map(function(field) {
       this.refs[field].value = '';
-    });
+    }.bind(this));
   }
 
   handleAdd(e) {
@@ -19,7 +19,7 @@ class AddProvider extends React.Component {
     e.preventDefault();
 
     // Important: key name must match syntax used in json data
-    const formValues = {
+    var formValues = {
       first_name: this.refs.fName.value,
       last_name: this.refs.lName.value,
       email_address: this.refs.email.value,
@@ -35,7 +35,7 @@ class AddProvider extends React.Component {
 
   render() {
     // Use an inline style rule to handle the Form display
-    const formDisplay = {
+    var formDisplay = {
       display: this.props.formIsVisible ? 'block' : 'none'
     };
 
